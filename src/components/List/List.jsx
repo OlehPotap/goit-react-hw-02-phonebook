f8844c22a1556d218d9761532e189688a70d6d0c
Oleg <potap.oleh.v@gmail.com>
2022-05-17 11:47:32 +0300

    HW2Updt

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,24 +1,29 @@
-import ContactsListItem from './ContactsListItem'
+import ContactsListItem from './ContactsListItem';
 import PropTypes from 'prop-types';
 
-const List = ({contacts, filteredData, onClick})=>{
-    return (
-        <ol className="list">
-            <ContactsListItem contacts={contacts} filteredData={filteredData} onClick={onClick}/>
-        </ol>
-    )
-}
+const List = ({ contacts, filteredData, onClick }) => {
+  return (
+    <ol className="list">
+      <ContactsListItem
+        contacts={contacts}
+        filteredData={filteredData}
+        onClick={onClick}
+      />
+    </ol>
+  );
+};
 
 List.propTypes = {
-    contacts: PropTypes.arrayOf(PropTypes.shape( {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
       id: PropTypes.string.isRequired,
       name: PropTypes.string,
       number: PropTypes.string,
-    })),
-  
-    filteredData: PropTypes.string,
-    onClick: PropTypes.func,
-  
-  }
+    })
+  ),
+
+  filteredData: PropTypes.string,
+  onClick: PropTypes.func,
+};
 
 export default List;
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,17 +1,13 @@
-import React from "react";
-import styles from '../Section/section.module.css'
-import PropTypes from "prop-types";
+import React from 'react';
+import styles from '../Section/section.module.css';
+import PropTypes from 'prop-types';
 
-const Section = ({children}) => {
-        return(
-            <section className={styles.section}>
-                {children}
-            </section>
-        )
-}
+const Section = ({ children }) => {
+  return <section className={styles.section}>{children}</section>;
+};
 
 Section.propTypes = {
-    children: PropTypes.arrayOf(PropTypes.element)
-}
+  children: PropTypes.arrayOf(PropTypes.element),
+};
 
 export default Section;
