75372ba63cce17fd8126efea1dbd888fad142c94
Oleg <potap.oleh.v@gmail.com>
2022-05-17 12:16:37 +0300

    IDK

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -48,7 +48,7 @@ class Form extends React.Component {
             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
-            // value={this.state.name}
+            value={this.state.name}
             onChange={this.handleChange}
           />
           <label htmlFor="number">Number</label>
@@ -59,7 +59,7 @@ class Form extends React.Component {
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
             required
-            // value={this.state.number}
+            value={this.state.number}
             onChange={this.handleChange}
           />
           <button type="submit" className={styles.button}>
